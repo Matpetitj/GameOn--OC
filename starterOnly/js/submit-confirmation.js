36acbfd6f367837728e8d8d4549ba5409b5b968b
Mathis Petitjean <100531763+Matpetitj@users.noreply.github.com>
2023-10-22 17:40:20 +0200

    style on confirmation modal

--- starterOnly/js/submit-confirmation.js
+++ starterOnly/js/submit-confirmation.js
@@ -3,16 +3,24 @@ function validationWindow(){
     form.style.display = 'none';
 
 //Création du nouveau texte dans la modal
+    const div = document.createElement('div');
+    div.setAttribute("class", "flexCenter");
+
     const validationText = document.createElement("p");
     validationText.className = "validationText";
-    validationText.textContent = "Formulaire envoyé ! ";
-    document.querySelector(".modal-body").appendChild(validationText);
-
+    validationText.classList.add= "centerText";
+    validationText.textContent = "Merci pour votre inscription";
+    div.appendChild(validationText)
+    
 //Création d'un nouveau bouton pour fermer la modal
     const buttonConfirmation = document.createElement("button");
     buttonConfirmation.className = "btn-signup button";
+    buttonConfirmation.classList.add = "center";
     buttonConfirmation.textContent = "Fermer";
-    document.querySelector(".modal-body").appendChild(buttonConfirmation);
+    
+    div.appendChild(buttonConfirmation);
+
+    document.querySelector(".modal-body").appendChild(div);
     buttonConfirmation.addEventListener('click', closeModal);
 }
 
